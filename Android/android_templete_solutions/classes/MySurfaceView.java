57cd29db9615217b9ace37e84a44d3d04e7c4b53
halohoop <halohoop@163.com>
2017-03-23 19:03:44 +0800

    update custom SurfaceView templete.

--- Android/android_templete_solutions/classes/MySurfaceView.java
+++ Android/android_templete_solutions/classes/MySurfaceView.java
@@ -1,4 +1,4 @@
-package com.tplink.tpcompass.widgets;
+package com.halohoop.widgets;
 
 import android.content.Context;
 import android.graphics.Canvas;
@@ -10,7 +10,15 @@ import android.view.SurfaceView;
  * Created by Pooholah on 2016/9/15.
  */
 
-public class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback {
+public class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback, Runnable {
+
+    //SurfaceHolder
+    private SurfaceHolder mHolder;
+    //用于绘图的Canvas
+    private Canvas mCanvas;
+    //子线程标志位
+    private boolean mIsDrawing;
+
     public MySurfaceView(Context context) {
         this(context, null);
     }
@@ -26,25 +34,24 @@ public class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback
     }
 
     private void init(Context context, AttributeSet attrs) {
-        getHolder().addCallback(this);
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+        setFocusable(true);
+        setFocusableInTouchMode(true);
+        this.setKeepScreenOn(true);
+        mHolder.setFormat(PixelFormat.OPAQUE);
     }
 
     private void initAttrs(AttributeSet attrs) {
         //TODO deal with attrs
     }
 
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-
-    }
-
+    //表示SurfaceView的创建，改变和销毁
     @Override
     public void surfaceCreated(SurfaceHolder surfaceHolder) {
-        //surfaceview templete
-        Canvas canvas = surfaceHolder.lockCanvas();
-        draw(canvas);//final to onDraw
-        surfaceHolder.unlockCanvasAndPost(canvas);
+        mIsDrawing = true;
+        //子线程重复绘制
+        new Thread(this).start();//见run方法
     }
 
     @Override
@@ -54,6 +61,27 @@ public class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
-
+        mIsDrawing = false;
     }
+
+    @Override
+    public void run(){
+        while(mIsDrawing){
+            draw();
+        }
+    }
+
+    private void draw(){
+        try {
+            mCanvas = mHolder.lockCanvas();
+            //draw something
+        }catch (Exception e){
+
+        }finally {
+            if(mCanvas!=null){
+                mHolder.unlockCanvasAndPost(mCanvas);//保证每次都能将内容提交
+            }
+        }
+    }
+
 }
